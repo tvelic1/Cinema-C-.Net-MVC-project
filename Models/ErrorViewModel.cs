9bd473e53092c98466347e2daa3b7ea40b13beed
zlendo1 <zlendo1@etf.unsa.ba>
2023-05-21 12:23:41 +0200

    generated ASP.NET project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOAD_G6_najjaci_tim.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
